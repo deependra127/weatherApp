77cb3672ff06d73d1519ebc89074404f032f682c
Deependra Singh <deependras@iitg.ac.in>
2025-09-04 23:17:49 +0530

    fixed png imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Weather from './Weather';
 import './appstyle.css';
+import metro from './assets/meteorology.png';
 
 const App = () => {
   return (
     <div id='container'>
       <header id='header'>
-        <div ><img src="src/assets/meteorology.png" alt="logo" id='logo'/></div>
+        <div ><img src= {metro} alt="logo" id='logo'/></div>
         <div id='name'><span id='header-special'>WEATHER</span> HUB</div>
       </header>
       <main id='main'>
